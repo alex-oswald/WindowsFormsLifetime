0b08fa8c119b011ffbce78f0f805ac1d490ea543
Alex Oswald <alex.oswald@outlook.com>
2021-11-19 21:37:19 -0800

    Move mvp project, add example generator project

---
+++ src/WindowsFormsLifetime.Mvp/MvpUserControlBase.cs
@@ -0,0 +1,21 @@
+﻿using OswaldTechnologies.Extensions.Hosting.WindowsFormsLifetime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsLifetime.Mvp
+{
+    public class MvpUserControlBase : UserControl
+    {
+        public MvpUserControlBase(
+            IGuiContext guiContext)
+        {
+            //InitializeComponent();
+            GuiContext = guiContext;
+        }
+
+        protected IGuiContext GuiContext { get; }
+    }
+}

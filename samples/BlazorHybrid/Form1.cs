e7f48d99eb66bc529bec1c0e25c627ecd0ce5cf5
Alex Oswald <alex.oswald@outlook.com>
2022-07-16 22:32:04 -0700

    Add BlazorHybrid sample app

---
+++ samples/BlazorHybrid/Form1.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorHybrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var services = new ServiceCollection();
+            services.AddWindowsFormsBlazorWebView();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<Counter>("#app");
+        }
+    }
+}

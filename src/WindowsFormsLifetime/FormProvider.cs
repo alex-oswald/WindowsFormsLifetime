bd703aa04c63cac1ef5d44743a8ebb1c4f364bc7
midare160 <43279447+midare160@users.noreply.github.com>
2023-03-14 00:30:44 +0100

    Update FormProvider.cs

--- src/WindowsFormsLifetime/FormProvider.cs
+++ src/WindowsFormsLifetime/FormProvider.cs
@@ -2,49 +2,52 @@
 
 namespace WindowsFormsLifetime
 {
-    public interface IFormProvider
-    {
-        /// <summary>
-        /// Gets the requested form type and ensures it is created on the UI thread.
-        /// </summary>
-        /// <typeparam name="T">The form type to get.</typeparam>
-        /// <returns>An instance of the form, asynchronously.</returns>
-        Task<T> GetFormAsync<T>() where T : Form;
+	public interface IFormProvider
+	{
+		/// <summary>
+		/// Gets the requested form type and ensures it is created on the UI thread.
+		/// </summary>
+		/// <typeparam name="T">The form type to get.</typeparam>
+		/// <returns>An instance of the form, asynchronously.</returns>
+		Task<T> GetFormAsync<T>() where T : Form;
 
-        Task<Form> GetMainFormAsync();
-    }
+		Task<Form> GetMainFormAsync();
+	}
 
-    public class FormProvider : IFormProvider
-    {
-        private readonly SemaphoreSlim _semaphore = new(1, 1);
-        private readonly IServiceProvider _serviceProvider;
-        private readonly IWindowsFormsSynchronizationContextProvider _syncContextManager;
+	public class FormProvider : IFormProvider, IDisposable
+	{
+		private readonly SemaphoreSlim _semaphore = new(1, 1);
+		private readonly IServiceProvider _serviceProvider;
+		private readonly IWindowsFormsSynchronizationContextProvider _syncContextManager;
 
-        public FormProvider(
-            IServiceProvider serviceProvider,
-            IWindowsFormsSynchronizationContextProvider syncContextManager)
-        {
-            _serviceProvider = serviceProvider;
-            _syncContextManager = syncContextManager;
-        }
+		public FormProvider(
+			IServiceProvider serviceProvider,
+			IWindowsFormsSynchronizationContextProvider syncContextManager)
+		{
+			_serviceProvider = serviceProvider;
+			_syncContextManager = syncContextManager;
+		}
 
-        public async Task<T> GetFormAsync<T>()
-            where T : Form
-        {
-            // We are throttling this because there is only one gui thread
-            await _semaphore.WaitAsync();
+		public async Task<T> GetFormAsync<T>()
+			where T : Form
+		{
+			// We are throttling this because there is only one gui thread
+			await _semaphore.WaitAsync();
 
-            var form = await _syncContextManager.SynchronizationContext.InvokeAsync(() => _serviceProvider.GetService<T>());
+			var form = await _syncContextManager.SynchronizationContext.InvokeAsync(() => _serviceProvider.GetService<T>());
 
-            _semaphore.Release();
+			_semaphore.Release();
 
-            return form;
-        }
+			return form;
+		}
 
-        public Task<Form> GetMainFormAsync()
-        {
-            var applicationContext = _serviceProvider.GetService<ApplicationContext>();
-            return Task.FromResult(applicationContext.MainForm);
-        }
-    }
+		public Task<Form> GetMainFormAsync()
+		{
+			var applicationContext = _serviceProvider.GetService<ApplicationContext>();
+			return Task.FromResult(applicationContext.MainForm);
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize", Justification = "<Pending>")]
+		public void Dispose() => _semaphore?.Dispose();
+	}
 }

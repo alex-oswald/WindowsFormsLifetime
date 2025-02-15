64dc90c8724bdd578d76fdfeddc5820892ec4dc1
Alex Oswald <alex.oswald@outlook.com>
2025-02-15 14:32:22 -0800

    Remove net 7

--- src/WindowsFormsLifetime/HostBuilderExtensions.cs
+++ src/WindowsFormsLifetime/HostBuilderExtensions.cs
@@ -41,63 +41,8 @@ public static class HostBuilderExtensions
         this IHostBuilder hostBuilder, Func<TStartForm, TAppContext> applicationContextFactory, Action<WindowsFormsLifetimeOptions> configure = null)
         where TAppContext : ApplicationContext
         where TStartForm : Form
-        => hostBuilder.ConfigureServices(services => services.AddWindowsFormsLifetime(applicationContextFactory, configure));
+        => hostBuilder.ConfigureServices(services => services.AddWindowsFormsLifetime<TAppContext, TStartForm>(applicationContextFactory, configure));
 
-#if NET7_0_OR_GREATER
-    /// <summary>
-    /// Enables Windows Forms support, builds and starts the host, starts the startup <see cref="Form"/>,
-    /// then waits for the startup form to close before shutting down.
-    /// </summary>
-    /// <param name="hostAppBuilder">The <see cref="HostApplicationBuilder" /> to configure.</param>
-    /// <param name="configure">The delegate for configuring the <see cref="WindowsFormsLifetimeOptions"/>.</param>
-    /// <returns>The same instance of the <see cref="HostApplicationBuilder"/> for chaining.</returns>
-    public static HostApplicationBuilder UseWindowsFormsLifetime<TStartForm>(
-        this HostApplicationBuilder hostAppBuilder, Action<WindowsFormsLifetimeOptions> configure = null)
-        where TStartForm : Form
-    {
-        hostAppBuilder.Services.AddWindowsFormsLifetime<TStartForm>(configure);
-
-        return hostAppBuilder;
-    }
-
-    /// <summary>
-    /// Enables Windows Forms support, builds and starts the host, starts the startup <see cref="ApplicationContext"/>,
-    /// then waits for the startup context to close before shutting down.
-    /// </summary>
-    /// <param name="hostAppBuilder">The <see cref="HostApplicationBuilder" /> to configure.</param>
-    /// <param name="applicationContextFactory">The <see cref="ApplicationContext"/> factory.</param>
-    /// <param name="configure">The delegate for configuring the <see cref="WindowsFormsLifetimeOptions"/>.</param>
-    /// <returns>The same instance of the <see cref="HostApplicationBuilder"/> for chaining.</returns>
-    public static HostApplicationBuilder UseWindowsFormsLifetime<TAppContext>(
-        this HostApplicationBuilder hostAppBuilder, Func<TAppContext> applicationContextFactory = null, Action<WindowsFormsLifetimeOptions> configure = null)
-        where TAppContext : ApplicationContext
-    {
-        hostAppBuilder.Services.AddWindowsFormsLifetime(applicationContextFactory, configure);
-
-        return hostAppBuilder;
-    }
-
-    /// <summary>
-    /// Enables Windows Forms support, builds and starts the host, starts the startup <see cref="ApplicationContext"/>,
-    /// then waits for the startup context to close before shutting down.
-    /// </summary>
-    /// <param name="hostAppBuilder">The <see cref="HostApplicationBuilder" /> to configure.</param>
-    /// <param name="applicationContextFactory">The <see cref="ApplicationContext"/> factory.</param>
-    /// <param name="configure">The delegate for configuring the <see cref="WindowsFormsLifetimeOptions"/>.</param>
-    /// <returns>The same instance of the <see cref="HostApplicationBuilder"/> for chaining.</returns>
-    public static HostApplicationBuilder UseWindowsFormsLifetime<TAppContext, TStartForm>(
-        this HostApplicationBuilder hostAppBuilder
-        , Func<TStartForm, TAppContext> applicationContextFactory, Action<WindowsFormsLifetimeOptions> configure = null)
-        where TAppContext : ApplicationContext
-        where TStartForm : Form
-    {
-        hostAppBuilder.Services.AddWindowsFormsLifetime(applicationContextFactory, configure);
-
-        return hostAppBuilder;
-    }
-#endif
-
-#if NET8_0_OR_GREATER
     /// <summary>
     /// Enables Windows Forms support, builds and starts the host, starts the startup <see cref="Form"/>,
     /// then waits for the startup form to close before shutting down.
@@ -149,5 +94,4 @@ public static class HostBuilderExtensions
 
         return hostAppBuilder;
     }
-#endif
 }
